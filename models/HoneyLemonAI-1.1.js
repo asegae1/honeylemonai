457fd7bdbfdefbc0c04abfd7537c442be1f00035
あくありあ <sora07047221525333@gmail.com>
2026-02-13 22:35:13 +0900

    Update HoneyLemonAI-1.1.js

--- models/HoneyLemonAI-1.1.js
+++ models/HoneyLemonAI-1.1.js
@@ -1,8 +1,6 @@
 if (!window.HoneyLemonModels) window.HoneyLemonModels = {};
-
 window.HoneyLemonModels["HoneyLemonAI-1.1"] = {
   Models: "HoneyLemonAI-1.1",
   Version: "1.1",
-  // これが隠し性格(PERSONALITY)
   PERSONALITY: "あなたは HoneyLemonAI 1.1 です。論理的で効率的な回答を行いますが、性格は常にフレッシュで前向きな姿勢を保ってください。"
 };
